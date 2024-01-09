03157d00d2fd26a9685bbe143f241e5855dc726b
LeviMF <levimferreira@outlook.com>
2024-01-08 21:23:01 -0300

    teste de Scanner
    Signed-off-by: LeviMF <levimferreira@outlook.com>

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 import Atendimento.Cliente;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         boolean teste = true;
-
+        
+        Scanner teclado = new Scanner(System.in);
+        
         Cliente teste1 = new Cliente("coqueiro", "MS", "quadra 4", "Mauro");
         teste1.solicitarOrcamento();
         System.out.println("Pedido chegou ? [Sim] [Nao]");
-        teste1.receberPedido(teste);
+        teste1.receberPedido(teclado.nextBoolean());
     }
 }
