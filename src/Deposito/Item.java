0b2fad1e1045993935f154d3dd9de29bed5de873
LeviMF <levimferreira@outlook.com>
2024-01-11 21:57:19 -0300

    criaçao da classe item

---
+++ src/Deposito/Item.java
@@ -0,0 +1,21 @@
+package Deposito;
+
+public class Item {
+    String escalaDeCor;
+    String tamanho;
+    String marca;
+    String descricao;
+    int codigo;
+    int registroAnvisa;
+
+    public Item(String escalaDeCor, String tamanho, String marca, String descricao, int codigo, int registroAnvisa) {
+        this.escalaDeCor = escalaDeCor;
+        this.tamanho = tamanho;
+        this.marca = marca;
+        this.descricao = descricao;
+        this.codigo = codigo;
+        this.registroAnvisa = registroAnvisa;
+    }
+
+
+}
